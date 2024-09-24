a4adf7afb39b43d9a4e080253e85b7146e71e9b0
hexs00si <shravanr.biz@gmail.com>
2024-09-24 11:35:46 +0530

    Add .gitignore to ignore unnecessary files

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/5. Exercise - Constant Or Variable.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/5. Exercise - Constant Or Variable.xcplaygroundpage/Contents.swift
@@ -10,7 +10,11 @@
  
  For each of the metrics above, declare either a constant or a variable and assign it a value corresponding to a hypothetical post. Be sure to use proper naming conventions.
  */
-
+var numberOfLikes = 12345
+var numberOfComments = 200
+let yearCreated = 2019
+let monthCreated = 6
+let dayCreated = 21
 
 /*:
 [Previous](@previous)  |  page 5 of 10  |  [Next: App Exercise - Fitness Tracker: Constant or Variable?](@next)
--- 1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -3,17 +3,20 @@
  
  Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking the variable name.
  */
-
-
+var firstDecimal : Double
+var seconDecimal : Double
 //:  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
+var trueOrFalse : Bool = true
+//firstDecimal = trueOrFalse
+print("tureOrfalse variable is of boolean type , and firstDecimal is of type double two variables of indifferent types can't be assigned to each other")
 //:  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
+var newString : String = "John"
+//newString = firstDecimal
+print("newString variable is of string type , and firstDecimal is of type double two variables of indifferent types can't be assigned to each other")
 //:  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
+var wholeNumber = 2
+//wholeNumber = firstDecimal
+print("wholeNumber variable is of Int type , and firstDecimal is of type double two variables of indifferent types can't be assigned to each other")
 /*:
 [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
  */
